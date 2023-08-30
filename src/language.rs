2e425a9ac8f2e93680e6dcf1f50134af56cb6c71
ErrorNoInternet <errornointernet@envs.net>
2023-08-30 11:25:02 +0800

    Minor changes

--- src/language.rs
+++ src/language.rs
@@ -1,20 +1,19 @@
+use crate::logging::{log_message, LogMessageType};
 use phf::phf_map;
 
-use crate::logging::{log_message, LogMessageType};
-
-static TEXT: phf::Map<&'static str, &'static str> = phf_map! {
+static TEXTS: phf::Map<&'static str, &'static str> = phf_map! {
     "pong" => "🏓 Pong!",
 };
 
 pub fn get_text(key: &str) -> &str {
-    match TEXT.get(key) {
+    match TEXTS.get(key) {
         Some(value) => value,
         None => {
             log_message(
                 LogMessageType::Error,
                 &format!("Missing text for \"{key}\"!"),
             );
-            "?"
+            key
         }
     }
 }
